cbc511ae7eb1ce3293fb0f673588e6878af605a8
Nick <nmelms92@gmail.com>
2023-01-07 23:25:25 -0500

    added remove

--- singly linked lists/singlyLinkedList.js
+++ singly linked lists/singlyLinkedList.js
@@ -101,6 +101,18 @@ class SinglyLinkedList {
     this.length++;
     return true;
   }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === this.length - 1) return this.pop();
+    if (index === 0) return this.shift();
+
+    let prev = this.get(index - 1);
+    let removed = prev.next;
+    prev.next = prev.next.next;
+    this.length--;
+    return removed;
+  }
 }
 
 let list = new SinglyLinkedList();
