856521d2e0bd4a259cfad6c162ac69f0b552550b
Nick <nmelms92@gmail.com>
2023-01-07 22:44:44 -0500

    created set

--- singly linked lists/singlyLinkedList.js
+++ singly linked lists/singlyLinkedList.js
@@ -79,6 +79,13 @@ class SinglyLinkedList {
 
     return current;
   }
+
+  set(index, val) {
+    let node = this.get(index);
+    if (node === undefined) return false;
+    node.val = val;
+    return true;
+  }
 }
 
 let list = new SinglyLinkedList();
