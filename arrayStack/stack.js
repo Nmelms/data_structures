e9ca743a64a1abcc5f3604d6a5201e26c98796a6
Nick <nmelms92@gmail.com>
2023-01-11 19:02:19 -0800

    stack array implementation

---
+++ arrayStack/stack.js
@@ -0,0 +1,12 @@
+//using push and pop on an array acts as a stack
+let stack = [];
+
+stack.push("google");
+stack.push("insta");
+stack.push("tyouube.com");
+
+stack.pop();
+stack.pop();
+stack.pop();
+
+//using shift and unshift works aswell
