1dc16a2cb0c16f4bf778fa3c3449ce7c21ca3337
Nick <nmelms92@gmail.com>
2023-01-21 23:33:04 -0800

    implemented get and set for hash tables

--- hash table/script.js
+++ hash table/script.js
@@ -1,11 +1,51 @@
-const hash = (key, arrLen) => {
-  let total = 0;
-  let prime = 31;
+// const hash = (key, arrLen) => {
+//   let total = 0;
+//   let prime = 31;
 
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i];
-    let value = char.charCodeAt(0) - 96;
-    total = (total * prime + value) % arrLen;
+//   for (let i = 0; i < Math.min(key.length, 100); i++) {
+//     let char = key[i];
+//     let value = char.charCodeAt(0) - 96;
+//     total = (total * prime + value) % arrLen;
+//   }
+//   return total;
+// };
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
-};
+
+  _hash(key) {
+    let total = 0;
+    let prime = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * prime + value) % 53;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    console.log(key);
+    let hash = this._hash(key);
+    this.keyMap[hash]
+      ? this.keyMap[hash].push([key, value])
+      : (this.keyMap[hash] = [[key, value]]);
+  }
+
+  get(key) {
+    let hash = this._hash(key);
+
+    if (this.keyMap[hash]) {
+      for (let i = 0; i < this.keyMap[hash].length; i++) {
+        if (this.keyMap[hash][i][0] === key) {
+          return this.keyMap[hash][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+}
+
+const table = new HashTable();
